bb99df06add0e592535ee5b427e56c8f954574c0
CodeBlooded-dot-exe <dudewheresmycamel@gmail.com>
2025-05-05 12:22:27 +0000

    new file: python.py

---
+++ python.py
@@ -0,0 +1,7 @@
+# This is a comment. Python ignores lines that start with #
+print("Hello, world!")  # This prints text to the screen
+
+# Let's do a simple calculation
+a = 5
+b = 3
+print("The sum is:", a + b)
